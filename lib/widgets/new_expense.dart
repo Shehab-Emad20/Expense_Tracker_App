4418ab2997712ec7ec51246e2f5917272763bf31
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-26 10:38:07 -0700

    Getting User Input on Every Keystroke

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,18 +8,30 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var enteredTitle = '';
+  void saveTitleInput(String inputValue) {
+    enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
+            onChanged: saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {}, child: const Text('Save Expense'))
+            ],
+          )
         ],
       ),
     );
