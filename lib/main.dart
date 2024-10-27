55affb5eb80b14a454b9dbc4b327e21dbf49720a
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-26 20:11:39 -0700

    Setting & Using a Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,22 @@ void main() {
   runApp(const ExpensesApp());
 }
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Expenses());
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: kColorScheme,
+            appBarTheme: AppBarTheme(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            )),
+        debugShowCheckedModeBanner: false,
+        home: const Expenses());
   }
 }
