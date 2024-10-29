9d6ff2064142ecaf22474c079dc1538ce5975d7a
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-28 19:18:09 -0700

    Locking the Device Orientiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:expense_tracker/widgets/expense_list/expenses.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then(
+  //   (fn) {},
+  // );
   runApp(const ExpensesApp());
 }
 
